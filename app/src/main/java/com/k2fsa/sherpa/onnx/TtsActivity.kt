6cb809a9de8aeee64fd82ead64d7a05332b839bb
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2024-05-05 18:13:18 +0300

    added tts activity

---
+++ app/src/main/java/com/k2fsa/sherpa/onnx/TtsActivity.kt
@@ -0,0 +1,289 @@
+package com.k2fsa.sherpa.onnx
+
+import android.content.res.AssetManager
+import android.media.AudioAttributes
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioTrack
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import io.github.leonidius20.lugat.R
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+const val TAG = "sherpa-onnx"
+
+class TtsActivity : AppCompatActivity() {
+    private lateinit var tts: OfflineTts
+    private lateinit var text: EditText
+    private lateinit var sid: EditText
+    private lateinit var speed: EditText
+    private lateinit var generate: Button
+    private lateinit var play: Button
+
+    // see
+    // https://developer.android.com/reference/kotlin/android/media/AudioTrack
+    private lateinit var track: AudioTrack
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tts)
+
+        Log.i(TAG, "Start to initialize TTS")
+        initTts()
+        Log.i(TAG, "Finish initializing TTS")
+
+        Log.i(TAG, "Start to initialize AudioTrack")
+        initAudioTrack()
+        Log.i(TAG, "Finish initializing AudioTrack")
+
+        text = findViewById(R.id.text)
+        sid = findViewById(R.id.sid)
+        speed = findViewById(R.id.speed)
+
+        generate = findViewById(R.id.generate)
+        play = findViewById(R.id.play)
+
+        generate.setOnClickListener { onClickGenerate() }
+        play.setOnClickListener { onClickPlay() }
+
+        sid.setText("0")
+        speed.setText("1.0")
+
+        // we will change sampleText here in the CI
+        val sampleText = ""
+        text.setText(sampleText)
+
+        play.isEnabled = false
+    }
+
+    private fun initAudioTrack() {
+        val sampleRate = tts.sampleRate()
+        val bufLength = AudioTrack.getMinBufferSize(
+            sampleRate,
+            AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_FLOAT
+        )
+        Log.i(TAG, "sampleRate: ${sampleRate}, buffLength: ${bufLength}")
+
+        val attr = AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .build()
+
+        val format = AudioFormat.Builder()
+            .setEncoding(AudioFormat.ENCODING_PCM_FLOAT)
+            .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
+            .setSampleRate(sampleRate)
+            .build()
+
+        track = AudioTrack(
+            attr, format, bufLength, AudioTrack.MODE_STREAM,
+            AudioManager.AUDIO_SESSION_ID_GENERATE
+        )
+        track.play()
+    }
+
+    // this function is called from C++
+    private fun callback(samples: FloatArray) {
+        track.write(samples, 0, samples.size, AudioTrack.WRITE_BLOCKING)
+    }
+
+    private fun onClickGenerate() {
+        val sidInt = sid.text.toString().toIntOrNull()
+        if (sidInt == null || sidInt < 0) {
+            Toast.makeText(
+                applicationContext,
+                "Please input a non-negative integer for speaker ID!",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
+        val speedFloat = speed.text.toString().toFloatOrNull()
+        if (speedFloat == null || speedFloat <= 0) {
+            Toast.makeText(
+                applicationContext,
+                "Please input a positive number for speech speed!",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
+        val textStr = text.text.toString().trim()
+        if (textStr.isBlank() || textStr.isEmpty()) {
+            Toast.makeText(applicationContext, "Please input a non-empty text!", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        track.pause()
+        track.flush()
+        track.play()
+
+        play.isEnabled = false
+        Thread {
+            val audio = tts.generateWithCallback(
+                text = textStr,
+                sid = sidInt,
+                speed = speedFloat,
+                callback = this::callback
+            )
+
+            val filename = application.filesDir.absolutePath + "/generated.wav"
+            val ok = audio.samples.size > 0 && audio.save(filename)
+            if (ok) {
+                runOnUiThread {
+                    play.isEnabled = true
+                    track.stop()
+                }
+            }
+        }.start()
+    }
+
+    private fun onClickPlay() {
+        val filename = application.filesDir.absolutePath + "/generated.wav"
+        val mediaPlayer = MediaPlayer.create(
+            applicationContext,
+            Uri.fromFile(File(filename))
+        )
+        mediaPlayer.start()
+    }
+
+    private fun initTts() {
+        var modelDir: String?
+        var modelName: String?
+        var ruleFsts: String?
+        var ruleFars: String?
+        var lexicon: String?
+        var dataDir: String?
+        var dictDir: String?
+        var assets: AssetManager? = application.assets
+
+        // The purpose of such a design is to make the CI test easier
+        // Please see
+        // https://github.com/k2-fsa/sherpa-onnx/blob/master/scripts/apk/generate-tts-apk-script.py
+        modelDir = "model"
+        modelName = "model.onnx"
+        ruleFsts = null
+        ruleFars = null
+        lexicon = null
+        dataDir = null
+        dictDir = null
+
+        // Example 1:
+        // modelDir = "vits-vctk"
+        // modelName = "vits-vctk.onnx"
+        // lexicon = "lexicon.txt"
+
+        // Example 2:
+        // https://github.com/k2-fsa/sherpa-onnx/releases/tag/tts-models
+        // https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-piper-en_US-amy-low.tar.bz2
+        // modelDir = "vits-piper-en_US-amy-low"
+        // modelName = "en_US-amy-low.onnx"
+        // dataDir = "vits-piper-en_US-amy-low/espeak-ng-data"
+
+        // Example 3:
+        // https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-icefall-zh-aishell3.tar.bz2
+        // modelDir = "vits-icefall-zh-aishell3"
+        // modelName = "model.onnx"
+        // ruleFsts = "vits-icefall-zh-aishell3/phone.fst,vits-icefall-zh-aishell3/date.fst,vits-icefall-zh-aishell3/number.fst,vits-icefall-zh-aishell3/new_heteronym.fst"
+        // ruleFars = "vits-icefall-zh-aishell3/rule.far"
+        // lexicon = "lexicon.txt"
+
+        // Example 4:
+        // https://k2-fsa.github.io/sherpa/onnx/tts/pretrained_models/vits.html#csukuangfj-vits-zh-hf-fanchen-c-chinese-187-speakers
+        // modelDir = "vits-zh-hf-fanchen-C"
+        // modelName = "vits-zh-hf-fanchen-C.onnx"
+        // lexicon = "lexicon.txt"
+        // dictDir = "vits-zh-hf-fanchen-C/dict"
+
+        // Example 5:
+        // https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-coqui-de-css10.tar.bz2
+        // modelDir = "vits-coqui-de-css10"
+        // modelName = "model.onnx"
+
+        if (dataDir != null) {
+            val newDir = copyDataDir(modelDir!!)
+            modelDir = newDir + "/" + modelDir
+            dataDir = newDir + "/" + dataDir
+            assets = null
+        }
+
+        if (dictDir != null) {
+            val newDir = copyDataDir(modelDir!!)
+            modelDir = newDir + "/" + modelDir
+            dictDir = modelDir + "/" + "dict"
+            ruleFsts = "$modelDir/phone.fst,$modelDir/date.fst,$modelDir/number.fst"
+            assets = null
+        }
+
+        val config = getOfflineTtsConfig(
+            modelDir = modelDir!!,
+            modelName = modelName!!,
+            lexicon = lexicon ?: "",
+            dataDir = dataDir ?: "",
+            dictDir = dictDir ?: "",
+            ruleFsts = ruleFsts ?: "",
+            ruleFars = ruleFars ?: "",
+        )!!
+
+        tts = OfflineTts(assetManager = assets, config = config)
+    }
+
+
+    private fun copyDataDir(dataDir: String): String {
+        Log.i(TAG, "data dir is $dataDir")
+        copyAssets(dataDir)
+
+        val newDataDir = application.getExternalFilesDir(null)!!.absolutePath
+        Log.i(TAG, "newDataDir: $newDataDir")
+        return newDataDir
+    }
+
+    private fun copyAssets(path: String) {
+        val assets: Array<String>?
+        try {
+            assets = application.assets.list(path)
+            if (assets!!.isEmpty()) {
+                copyFile(path)
+            } else {
+                val fullPath = "${application.getExternalFilesDir(null)}/$path"
+                val dir = File(fullPath)
+                dir.mkdirs()
+                for (asset in assets.iterator()) {
+                    val p: String = if (path == "") "" else path + "/"
+                    copyAssets(p + asset)
+                }
+            }
+        } catch (ex: IOException) {
+            Log.e(TAG, "Failed to copy $path. $ex")
+        }
+    }
+
+    private fun copyFile(filename: String) {
+        try {
+            val istream = application.assets.open(filename)
+            val newFilename = application.getExternalFilesDir(null).toString() + "/" + filename
+            val ostream = FileOutputStream(newFilename)
+            // Log.i(TAG, "Copying $filename to $newFilename")
+            val buffer = ByteArray(1024)
+            var read = 0
+            while (read != -1) {
+                ostream.write(buffer, 0, read)
+                read = istream.read(buffer)
+            }
+            istream.close()
+            ostream.flush()
+            ostream.close()
+        } catch (ex: Exception) {
+            Log.e(TAG, "Failed to copy $filename, $ex")
+        }
+    }
+}
